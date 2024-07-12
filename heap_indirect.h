11f9f990c3c51a42dafcf2641d523184e93c4e91
Olivier Langlois <olivier@trillion01.com>
2024-07-12 12:22:42 -0400

    initial commit

---
+++ heap_indirect.h
@@ -0,0 +1,215 @@
+#ifndef HEAP_INDIRECT_H_
+#define HEAP_INDIRECT_H_
+/*
+ * Indirect Heap algorithms
+ *
+ * pretty much a copy of the standard heap algorithms except that they
+ * are augmented by storing the element position when it is moved in the
+ * heap to fix a heap condition violation.
+ *
+ * This allows to efficiently implement the delete operation on an
+ * arbitrary element instead of reserving the deletion to only the root.
+ *
+ * Olivier Langlois - July 11, 2024
+ */
+
+#include <iterator>
+
+/*
+ * uncomment to preserve the heap elements stability. Preserving stability
+ * increases the number of operations to fix the heap conditions violation
+ * following a removal or a deletion.
+ */
+//#define BASE_HEAP_PRESERVE_STABILITY 1
+
+namespace Base {
+
+/*
+ * the root of the heap is the highest priority element
+ * when an element is popped, it is the first element is moved in the last
+ * element and the last element is placed in its right place by calling
+ * downheap.
+ */
+namespace HeapHelpers {
+template<typename RandomAccessIterator,
+         typename Distance,
+         typename itemType,
+         typename Compare>
+constexpr void
+upheap(RandomAccessIterator first,
+       Distance k, Distance topIndex,
+       itemType v, Compare & comp)
+{
+    Distance parent{(k - 1) / 2};
+
+    while (k > topIndex && // sentinel
+           comp(*(first + parent), v)) { // if v is greater (if comp is less)
+        auto it{first + k};
+
+        // move down the parent
+        *(it) = std::move(*(first + parent));
+        setHeapIndex(*it, k);
+        k = parent;
+        parent = (k - 1) / 2;
+    }
+    auto it{first + k};
+
+    *(it) = std::move(v);
+    setHeapIndex(*it, k);
+}
+
+template<typename RandomAccessIterator, typename Distance,
+         typename itemType, typename Compare>
+constexpr void
+downheap(RandomAccessIterator first,
+         const Distance topIndex, Distance k, Distance len,
+         itemType v, Compare comp)
+{
+    /*
+     * initialize secondChild with k to start inspecting its children in the
+     * following loop
+     */
+    Distance secondChild = k;
+
+    // move up the best child
+    while (secondChild < (len - 1) / 2) {
+        secondChild = 2 * (secondChild + 1);
+        // pick the biggest child
+        if (comp(*(first + secondChild),
+                 *(first + (secondChild - 1))))
+            --secondChild;
+#ifndef BASE_HEAP_PRESERVE_STABILITY
+        if (!comp(v, *(first + secondChild)))
+            break;
+#endif
+        auto it{first + k};
+
+        *(it) = std::move(*(first + secondChild));
+        setHeapIndex(*it, k);
+        k = secondChild;
+    }
+#ifndef BASE_HEAP_PRESERVE_STABILITY
+    auto it{first + k};
+
+    *(it) = std::move(v);
+    setHeapIndex(*it, k);
+#else
+    /*
+     * if heap size is odd
+     * and secondchild points on the leaves on the previous generation:
+     * ie: len=15, secondChild=6,
+     *
+     * place the last heap element in the previous generation unique leaf
+     * so that the upheap starts on the latest generation.
+     */
+    if ((len & 1) == 0 &&
+        secondChild == (len - 2) / 2) {
+        secondChild = 2 * (secondChild + 1);
+        const auto secondChildIdx{secondChild - 1};
+        auto it{first + k};
+
+        *(it) = std::move(*(first + secondChildIdx));
+        setHeapIndex(*it, k);
+        k = secondChildIdx;
+    }
+
+    /*
+     * at this point, k points to the hole, upheap the heap
+     * with v from this point.
+     *
+     * NOTE:
+     * This seems like downheap() could be simplified by following
+     * the algorithm in Robert Sedgewick book.
+     *
+     * This needs to be tried once unit tests are in place.
+     * this is done to preserve stability.
+     */
+    upheap(first, k, topIndex, std::move(v), comp);
+#endif
+}
+
+template<typename RandomAccessIterator, typename Compare>
+constexpr inline void
+remove(RandomAccessIterator first, RandomAccessIterator last,
+       RandomAccessIterator popPos, RandomAccessIterator result,
+       Compare& comp)
+{
+    using ValueType = typename std::iterator_traits<RandomAccessIterator>::value_type;
+    using DistanceType = typename std::iterator_traits<RandomAccessIterator>::difference_type;
+
+    /*
+     * previous smallest priority element is stored in v
+     * to be repositioned.
+     */
+    ValueType v{std::move(*result)};
+
+    // the current root is going to be popped
+    *result = std::move(*first);
+    downheap(first, DistanceType{},
+             DistanceType{popPos - first},
+             DistanceType(last - first),
+             std::move(v), comp);
+}
+}
+
+/**
+ *  @brief  Push an element onto a heap using comparison functor.
+ *  @param  first  Start of heap.
+ *  @param  last   End of heap + element.
+ *  @param  comp   Comparison functor.
+ *  @ingroup heap_algorithms
+ *
+ *  This operation pushes the element at last-1 onto the valid
+ *  heap over the range [first,last-1).  After completion,
+ *  [first,last) is a valid heap.  Compare operations are
+ *  performed using comp.
+*/
+template<typename RandomAccessIterator, typename Compare>
+constexpr inline void
+push_heap(RandomAccessIterator first, RandomAccessIterator last,
+          Compare comp)
+{
+    using ValueType = typename std::iterator_traits<RandomAccessIterator>::value_type;
+    using DistanceType = typename std::iterator_traits<RandomAccessIterator>::difference_type;
+    ValueType value = std::move(*(last - 1));
+
+    HeapHelpers::upheap(first, DistanceType((last - first) - 1),
+                        DistanceType{}, std::move(value), comp);
+}
+
+/**
+ *  @brief  Pop an element off a heap using comparison functor.
+ *  @param  first  Start of heap.
+ *  @param  last   End of heap.
+ *  @param  comp   Comparison functor to use.
+ *  @ingroup heap_algorithms
+ *
+ *  This operation pops the top of the heap.  The elements first
+ *  and last-1 are swapped and [first,last-1) is made into a
+ *  heap.  Comparisons are made using comp.
+ */
+template<typename RandomAccessIterator, typename Compare>
+constexpr inline void
+pop_heap(RandomAccessIterator first,
+         RandomAccessIterator last, Compare comp)
+{
+    if (last - first > 1) {
+        --last;
+        HeapHelpers::remove(first, last, first, last, comp);
+    }
+}
+
+template<typename RandomAccessIterator, typename Compare>
+constexpr inline void
+pop_heap(RandomAccessIterator first, RandomAccessIterator last,
+         RandomAccessIterator popPos,
+         Compare comp)
+{
+    if (last - first > 1) {
+        --last;
+        HeapHelpers::remove(first, last, popPos, last, comp);
+    }
+}
+}
+
+#endif
---
+++ heap_indirect_test.cpp
@@ -0,0 +1,218 @@
+/*
+ * heap indirect test
+ *
+ * Olivier Langlois July 12, 2024
+ *
+ * to compile:
+ * g++ -std=c++26 -g heap_indirect_test.cpp
+ */
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include "heap_indirect.h"
+
+namespace Base {
+/*
+ * class pointer_iterator
+ *
+ * iterator proxy that returns the address of the underlying iterator returned
+ * value
+ *
+ * this class is using a C++20 feature:
+ * https://en.cppreference.com/w/cpp/language/default_comparisons
+ */
+template <class iterator>
+class pointer_iterator
+{
+public:
+    pointer_iterator(iterator it)
+    : m_it(it) {}
+
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type   = std::ptrdiff_t;
+    using value_type        = typename std::iterator_traits<iterator>::pointer;
+    using pointer           = value_type*;
+    using reference         = value_type&;
+
+    value_type operator*() const { return &(*m_it); }
+    // Prefix increment
+    pointer_iterator& operator++() { ++m_it; return *this; }  
+
+    // Postfix increment
+    pointer_iterator operator++(int) { auto tmp{*this}; ++(*this); return tmp; }
+
+    friend bool operator== (const pointer_iterator &lhs,
+                            const pointer_iterator &rhs) { return lhs.m_it == rhs.m_it; };
+
+private:
+    iterator m_it;
+};
+}
+
+template <typename ValType>
+struct TestElem
+{
+    ValType v;
+    uint8_t pos{};
+};
+
+/*
+ * provide function required by Base::push_heap(), Base::pop_heap()
+ */
+template <typename ValType>
+inline void setHeapIndex(TestElem<ValType> *e, size_t idx)
+{
+    e->pos = idx;
+}
+
+template <typename Iterator>
+void printTestVec(Iterator first, Iterator last)
+{
+    std::for_each(first, last, [](const auto &curItem){
+        std::cout << curItem.v << ' ';
+    });
+    std::cout << '\n';
+    std::for_each(first, last, [](const auto &curItem){
+        std::cout << static_cast<unsigned>(curItem.pos) << ' ';
+    });
+    std::cout << '\n';
+}
+
+template <typename Iterator>
+void printPtrTestVec(Iterator first, Iterator last)
+{
+    std::for_each(first, last, [](const auto *curItem){
+        std::cout << curItem->v << ' ';
+    });
+    std::cout << '\n';
+    std::for_each(first, last, [](const auto *curItem){
+        std::cout << static_cast<unsigned>(curItem->pos) << ' ';
+    });
+    std::cout << '\n';
+}
+
+int main(int argc, char *argv[])
+{
+    auto comp{[](const auto *lhs, const auto *rhs){ return lhs->v > rhs->v; }};
+    std::vector<TestElem<int> > intVec{ {1}, {5}, {2}, {6}, {4}, {8}, {7}, {3} };
+    std::vector<TestElem<int> *> intPtrVec(Base::pointer_iterator{std::begin(intVec)},
+                                           Base::pointer_iterator{std::end(intVec)});
+    auto ipit{std::begin(intPtrVec)};
+    auto ivit{std::begin(intVec)};
+    auto intCmp{[](const TestElem<int> *lhs, const TestElem<int> *rhs){
+        return lhs->v < rhs->v;
+    }};
+
+    /*
+     * test case idea coming from the book:
+     * Algorithms in C++ by Robert Sedgewick (1992)
+     * Chapter 11 - Priority Queues, Exercise #1
+     */
+    std::cout << "insert(1),insert(5):\n";
+    Base::push_heap(ipit, ipit+2, intCmp);
+    printTestVec(ivit, ivit+2);
+
+    std::cout << "\ninsert(2):\n";
+    Base::push_heap(ipit, ipit+3, intCmp);
+    printTestVec(ivit, ivit+3);
+
+    std::cout << "\ninsert(6):\n";
+    Base::push_heap(ipit, ipit+4, intCmp);
+    printTestVec(ivit, ivit+4);
+
+    std::cout << "\nreplace(4):\n";
+    Base::push_heap(ipit, ipit+5, intCmp);
+    printPtrTestVec(ipit, ipit+5);
+    Base::pop_heap(ipit, ipit+5, intCmp);
+    std::cout << '\n';
+    printPtrTestVec(ipit, ipit+4);
+
+    std::cout << "\ninsert(8):\n";
+    *(ipit+4) = &intVec[5];
+    Base::push_heap(ipit, ipit+5, intCmp);
+    printPtrTestVec(ipit, ipit+5);
+
+    std::cout << "\nremove:\n";
+    Base::pop_heap(ipit, ipit+5, intCmp);
+    printPtrTestVec(ipit, ipit+4);
+
+    std::cout << "\ninsert(7):\n";
+    *(ipit+4) = &intVec[6];
+    Base::push_heap(ipit, ipit+5, intCmp);
+    printPtrTestVec(ipit, ipit+5);
+
+    std::cout << "\ninsert(3):\n";
+    *(ipit+5) = &intVec[7];
+    Base::push_heap(ipit, ipit+6, intCmp);
+    printPtrTestVec(ipit, ipit+6);
+
+    /*
+     * test case idea coming from the book:
+     * Algorithms in C++ by Robert Sedgewick (1992)
+     * Chapter 11 - Priority Queues, Exercise #3
+     */
+    std::vector<TestElem<char> > charVec{ {'E'}, {'A'}, {'S'}, {'Y'},
+                                          {'Q'}, {'U'}, {'E'}, {'S'}, {'T'}, {'I'}, {'O'}, {'N'} };
+    std::vector<TestElem<char> *> charPtrVec(Base::pointer_iterator{std::begin(charVec)},
+                                        Base::pointer_iterator{std::end(charVec)});
+    auto cpit{std::begin(charPtrVec)};
+    auto cvit{std::begin(charVec)};
+    auto charCmp{[](const TestElem<char> *lhs, const TestElem<char> *rhs){
+        return lhs->v < rhs->v;
+    }};
+
+    for (size_t offset{2}; offset <= 12; ++offset) {
+        std::cout << "\ninsert(" << charVec[offset-1].v << "):\n";
+        Base::push_heap(cpit, cpit+offset, charCmp);
+        printPtrTestVec(cpit, cpit+offset);
+    }
+    auto origCharVec{charVec};
+    auto origCharPtrVec{charPtrVec};
+
+    /*
+     * here we are cheating a bit because we know the element position
+     * that we want to pop.
+     *
+     * The use case that I had in mind is an object that wants to remove
+     * itself from the queue, by having its position in the queue, it can
+     * do it. (first_iterator+pos)
+     *
+     * This idea came to me when I wanted to convert an intrusive doubly linked
+     * list system to a priority queue.
+     *
+     * Before the refactoring, all the object had to do to remove itself from
+     * the list was to fix its own pointers and the ones from its detaching
+     * neighbors.
+     *
+     * deletion complexity has changed from C to NlogN but the speed gain
+     * from the insertions far outweights this loss.
+     */
+    std::cout << "\nremove E at pos 6:\n";
+    Base::pop_heap(cpit, cpit+12, cpit+6, charCmp);
+    printPtrTestVec(cpit, cpit+11);
+
+    charVec    = origCharVec;
+    charPtrVec = origCharPtrVec;
+
+    std::cout << "\nremove N at pos 5:\n";
+    Base::pop_heap(cpit, cpit+12, cpit+5, charCmp);
+    printPtrTestVec(cpit, cpit+11);
+
+    charVec    = origCharVec;
+    charPtrVec = origCharPtrVec;
+
+    std::cout << "\nremove U at pos 2:\n";
+    Base::pop_heap(cpit, cpit+12, cpit+2, charCmp);
+    printPtrTestVec(cpit, cpit+11);
+
+    charVec    = origCharVec;
+    charPtrVec = origCharPtrVec;
+
+    std::cout << "\nremove T at pos 1:\n";
+    Base::pop_heap(cpit, cpit+12, cpit+1, charCmp);
+    printPtrTestVec(cpit, cpit+11);
+    
+    return 0;
+}
